584d81c3cdc39ab920ddd24c67d9994462e553b4
brzsamu1407 <116824649+brzsamu1407@users.noreply.github.com>
2024-05-16 22:56:36 +0200

    improved camera rotation

--- Player/CameraController.gd
+++ Player/CameraController.gd
@@ -15,13 +15,10 @@ func _input(event):
 	if Input.is_action_just_pressed("scroll_up") and camera_3d.size >= 5:
 		camera_3d.size -= 2.5
 	
-	if Input.is_action_just_pressed("ui_left"):
-		if my_tween != null:
-			my_tween.kill()
-		my_tween = create_tween()
-		my_tween.tween_property(self,"rotation_degrees:y",rotation_degrees.y + 45,0.2)
-	elif Input.is_action_just_pressed("ui_right"):
-		if my_tween != null:
-			my_tween.kill()
-		my_tween = create_tween()
-		my_tween.tween_property(self,"rotation_degrees:y",rotation_degrees.y - 45,0.2)
+	
+
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		rotation_degrees.y -= 60 * delta
+	elif Input.is_action_pressed("ui_right"):
+		rotation_degrees.y += 60 * delta
